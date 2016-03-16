31597e605b254e86f8af0b5cf4b79a802e3d1eda
Duknam Yoo <yoo2001818@gmail.com>
2016-03-16 19:08:54 +0900

    Implement resizing

--- src/client.js
+++ src/client.js
@@ -1,11 +1,22 @@
 import createStore from './store/game';
 import RenderView from './view/render';
 
+document.body.style.padding = '0';
+document.body.style.margin = '0';
+
 // Create canvas
 let canvas = document.createElement('canvas');
 document.body.appendChild(canvas);
-canvas.width = 800;
-canvas.height = 800;
+canvas.width = ((window.innerWidth - 2) / 2 | 0) * 2;
+canvas.height = ((window.innerHeight - 2) / 2 | 0) * 2;
+
+window.addEventListener('resize', () => {
+  window.requestAnimationFrame(() => {
+    canvas.width = ((window.innerWidth - 2) / 2 | 0) * 2;
+    canvas.height = ((window.innerHeight - 2) / 2 | 0) * 2;
+    view.render();
+  });
+});
 
 let store = createStore(canvas);
 
