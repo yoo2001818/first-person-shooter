4b9c45959fd12ae80e58f5d0084312ebc68075bb
Duknam Yoo <yoo2001818@gmail.com>
2016-04-17 23:57:39 +0900

    Implement matrix cleanup on entity removal

--- src/game/system/matrix.js
+++ src/game/system/matrix.js
@@ -1,4 +1,5 @@
 import * as TransformChanges from '../change/transform';
+import * as ECSChanges from 'ecsalator/lib/ecs/changes';
 import { mat4 as Mat4 } from 'gl-matrix';
 
 export default class MatrixSystem {
@@ -37,7 +38,16 @@ export default class MatrixSystem {
       const { entity } = event.data;
       this.updateMatrix(entity);
     });
-    // TODO entity remove
+    store.changes.on(ECSChanges.REMOVE, (event) => {
+      const { entity, key } = event.data;
+      if (key !== 'transform') return;
+      this.removeMatrix(entity);
+    });
+    store.changes.on(ECSChanges.ENTITY_REMOVE, (event) => {
+      const { entity } = event.data;
+      if (entity.transform == null) return;
+      this.removeMatrix(entity);
+    });
   }
   updateMatrix(entity) {
     let matrix = this.matrices[entity.id];
@@ -47,6 +57,9 @@ export default class MatrixSystem {
     Mat4.fromRotationTranslation(matrix,
       entity.transform.rotation, entity.transform.position);
     Mat4.scale(matrix, matrix, entity.transform.scale);
+  }
+  removeMatrix(entity) {
+    delete this.matrices[entity.id];
   }
   get(entityId) {
     return this.matrices[entityId];
